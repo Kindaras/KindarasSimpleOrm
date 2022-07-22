aa5c125765f527ed11a42914aca91ee7a1eb8850
Matteo Conti <matteo.conti7@gmail.com>
2022-07-22 23:41:16 +0200

    commit

---
+++ app/src/main/java/com/kindaras/objectdatabase/test/TestObj.java
@@ -0,0 +1,49 @@
+package com.kindaras.objectdatabase.test;
+
+public class TestObj {
+    private String string;
+    private int integer;
+    private boolean aBoolean;
+    private Byte bb;
+    private Byte[] bArray;
+
+    public String getString() {
+        return string;
+    }
+
+    public void setString(String string) {
+        this.string = string;
+    }
+
+    public int getInteger() {
+        return integer;
+    }
+
+    public void setInteger(int integer) {
+        this.integer = integer;
+    }
+
+    public boolean isaBoolean() {
+        return aBoolean;
+    }
+
+    public void setaBoolean(boolean aBoolean) {
+        this.aBoolean = aBoolean;
+    }
+
+    public Byte getBb() {
+        return bb;
+    }
+
+    public void setBb(Byte bb) {
+        this.bb = bb;
+    }
+
+    public Byte[] getbArray() {
+        return bArray;
+    }
+
+    public void setbArray(Byte[] bArray) {
+        this.bArray = bArray;
+    }
+}
